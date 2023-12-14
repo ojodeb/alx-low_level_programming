085ae8c011be00cd3e6634c6b6a01626b48e7120
llibedojo <ojodebill@gmail.com>
2023-12-14 17:38:43 +0300

    two

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/*Program that prints Programming is like building a multilingual puzzle, return 0*/
+int main(0)
+{
+	puts(Programming is like building a multilingual puzzle\n);
+	return (0);
+}
