2944c86a3e6ecb6b753633d72a8781ba7c40b3fa
llibedojo <ojodebill@gmail.com>
2023-12-19 11:11:53 +0300

    fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,22 +15,14 @@ int main(void)
 	srand(time(NULL));
 
 	/* Generate a random number between -100 and 100 */
-	n = rand() % 201 - 100;
-
-	printf("The number %d is", n);
+	n = rand() % 199 - 99;
 
 	if (n > 0)
-	{
-		printf(" positive\n");
-	}
+		printf("%d is positive\n", n);
 	else if (n < 0)
-	{
-		printf(" negative\n");
-	}
+		printf("%d is negative\n", n);
 	else
-	{
-		printf(" zero\n");
-	}
+		printf("%d is zero\n", n);
 
 	return (0);
 }
