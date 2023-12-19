d1aaeb0025ea16fddec51102c854af5ec0829f3d
llibedojo <ojodebill@gmail.com>
2023-12-19 06:06:14 +0300

    C Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,11 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     /* Use puts to print the specified string */
     puts("Programming is like building a multilingual puzzle");
-
     return (0);
 }
 
