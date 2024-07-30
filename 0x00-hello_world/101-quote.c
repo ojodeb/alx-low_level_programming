a19b0c2e3b8b6b51b0041f7a8369dd3dc8ce6a75
llibedojo <ojodebill@gmail.com>
2024-07-30 14:25:17 +0300

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,9 @@
 #include <unistd.h>
 
 int main(void)
-{
-    write(2, "and that piece of art is useful\n"
-                 "- Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+{ 
+	char message[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+
+	write(2,message,sizeof(message) - 1);
+	return 1;
 }
