23ce1cad65fe3e0b372b8285f30bfbb104423cdd
llibedojo <ojodebill@gmail.com>
2024-07-30 14:14:30 +0300

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
-#include <unistd.h>
+#include <unistd.h> // Include this header for the write function
 
-int main(void)
-{
-    write(2, "and that piece of art is useful\n"
-                 "- Dora Korpar, 2015-10-19\n", 59);
-    return (1);
+int main(void) {
+    // The string to output, followed by a newline character
+    char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    // Using the write function to output the message to standard error (file descriptor 2)
+    write(2, message, sizeof(message) - 1);
+
+    // Return 1 as required
+    return 1;
 }
+
